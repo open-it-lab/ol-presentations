c3bd7ad498a84d9960c48ec33a81e08865ab1d2e
Jarrell Waggoner <malloc47@gmail.com>
2012-09-25 08:16:30 -0400

    more changes to education slides

--- education.sh
+++ education.sh
@@ -1,2 +1,2 @@
 #impressive -I education.info -x -c persistent developers/developer-presentation.pdf educational-resources.pdf involvement/involvement.pdf
-impressive -I education.info -x -c persistent short-open-source-intro.pdf educational-resources.pdf involvement/involvement.pdf
+impressive -I education.info -x -c persistent short-open-source-intro.pdf educational-resources.pdf involvement/involvement.pdf scmta.pdf
